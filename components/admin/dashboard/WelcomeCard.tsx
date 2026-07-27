549d7b38181f227ee106750fe130a3ef63dcd5b6
rismavi <erisrahma198@gmail.com>
2026-07-27 11:49:52 +0700

    revisi welcome card

--- components/admin/dashboard/WelcomeCard.tsx
+++ components/admin/dashboard/WelcomeCard.tsx
@@ -3,25 +3,28 @@ import { Plus } from "lucide-react";
 
 export default function WelcomeCard() {
   return (
-    <div className="rounded-[28px]  bg-[#F5A000]0p-8 text-[#1E1E1E] shadow-lg">
-      <h2 className="text-[40px] font-bold leading-tight">
-        Publish New
-        <br />
-        Brochure
-      </h2>
+    <div className="flex h-[420px] flex-col rounded-[28px] bg-[#F5A000] p-8 text-[#1E1E1E] shadow-lg">
 
-      <p className="mt-5 text-[17px] leading-8 text-[#4E3B14]">
-        Update your catalog with the latest fiber optics packages and
-        promotional offers.
-      </p>
+      <div>
+        <h2 className="text-[40px] font-bold leading-tight">
+          Publish New
+          <br />
+          Brochure
+        </h2>
+
+        <p className="mt-5 text-[17px] leading-8 text-[#4E3B14]">
+          Tambahkan brosur dan paket baru internet NARATEL
+        </p>
+      </div>
 
       <Link
         href="/admin/brochures/create"
-        className="mt-10 flex h-16 w-full items-center justify-center rounded-2xl bg-[#9A6500] text-xl font-semibold text-white shadow-md transition hover:bg-[#855700]"
+        className="mt-auto flex h-16 w-full items-center justify-center rounded-2xl bg-[#9A6500] text-xl font-semibold text-white shadow-md transition hover:bg-[#855700]"
       >
         <Plus size={24} className="mr-2" />
         Add Brochure
       </Link>
+
     </div>
   );
 }
